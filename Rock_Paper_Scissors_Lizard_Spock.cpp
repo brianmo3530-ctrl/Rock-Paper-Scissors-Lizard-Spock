67c2dbabec5a0c615971ec3d5a1b9d95b382e6b9
Brian <brianmo3530@gmail.com>
2026-01-11 12:03:29 -0600

    Update Rock_Paper_Scissors_Lizard_Spock.cpp
    finished options for player and computer choice

--- Rock_Paper_Scissors_Lizard_Spock.cpp
+++ Rock_Paper_Scissors_Lizard_Spock.cpp
@@ -19,3 +19,25 @@ int main() {
     std::cout << "3) ✌️  Scissors\n";
     std::cout << "4) 🦎  Lizard\n";
     std::cout << "5) 🖖  Spock\n";
+
+    std::cout << "shoot! ";
+    std::cin >> user;
+
+    if (user == 1) std::cout << "YOU: ✊ Rock\n";
+    else if (user == 2) std::cout << "YOU: ✋ Paper\n";
+    else if (user == 3) std::cout << "YOU: ✌️ Scissors\n";
+    else if (user == 4) std::cout << "YOU: 🦎 Lizard\n";
+    else if (user == 5) std::cout << "YOU: 🖖 Spock\n";
+    else {
+        std::cout << "Invalid option\n";
+        return 0;
+    }
+    if (computer == 1) std::cout << "COMPUTER: ✊ Rock\n";
+    else if (computer == 2) std::cout << "COMPUTER: ✋ Paper\n";
+    else if (computer == 3) std::cout << "COMPUTER: ✌️ Scissors\n";
+    else if (computer == 4) std::cout << "COMPUTER: 🦎 Lizard\n";
+    else if (computer == 5) std::cout << "COMPUTER: 🖖 Spock\n";
+
+    return 0;
+}
+
